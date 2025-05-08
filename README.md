10f96ea82ad0e3a6196410733943e13fddfc0ea7
Kamran Akbar <76488599+KamranAkbarKami@users.noreply.github.com>
2025-05-08 07:35:20 +0000

    Variables and Data Types

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # javascript_complete
 Javascript Complete for Learning
-Date Started:  MAy 8, 2025
+Date Started:  May 8, 2025
---
+++ variables_01/variables_class_1.js
@@ -0,0 +1,8 @@
+const value = 1
+let myName = "Kamran"
+console.log(value);
+console.log(myName);
+let  myTestName
+myTestName = "String"
+myTestName = 4
+console.log(myTestName)
